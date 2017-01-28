f5874dbe09a66f4973ae9b065582bfbee79fdb77
rohangokhale <gokhaler@oregonstate.edu>
2017-01-28 10:12:02 -0500

    Update alg3.py

--- alg3.py
+++ alg3.py
@@ -10,34 +10,40 @@ def divcon(A, first, last):#note: the 'last' element is not checked
             return (A[first], first, last)
         else:
             return (0, first, first)
+
     else:
         middle = (first + last) // 2 #divide
         #get the maximum sum from 0 to middle
         sum = 0
-        maxleft = (0, middle)   #stores the greatest sum up til that point, and the position where that subarray 'ends' on the left
-        for i in range(middle-1, first-1, -1):
-            sum += A[i]
-            if sum > maxleft[0]:
-                maxleft = (sum,i)
+        indexleft = middle #index of where the greatest subarray 'ends'
+        maxleft = 0  #stores the greatest sum up til that point
+        for n in range(middle-1, first-1, -1):
+            sum += A[n]
+            if sum > maxleft:
+                maxleft = sum
+                indexleft = n
                 
         #get the maximum sum from middle to the end
-        sum = 0
-        maxright = (0, middle) #stores the greatest sum up til that point, and the position where that subarray 'ends' on the right
-        for j in range(middle+1, last+1, +1):
-            sum += A[j-1]
-            if sum > maxright[0]:
-                maxright = (sum,j)
+        sum = 0 #reset the sum
+        indexright = middle #index of where the greatest subarray 'ends'
+        maxright = 0 #stores the greatest sum up til that point
+        for i in range(middle+1, last+1, +1):
+            sum += A[i-1]
+            if sum > maxright:
+                maxright = sum
+                indexright = i
 
-        overlap = (maxleft[0]+maxright[0], maxleft[1], maxright[1])
+        #take the overlap of the two max subarrays on each side
+        overlap = (maxleft+maxright, indexleft, indexright)
+        #find the max subarray out of the left, right, and 'middle' sections
+        maxsub = max(divcon(A, first, middle), divcon(A, middle, last), overlap)
+        return maxsub
 
-        return max(divcon(A, first, middle),
-                   divcon(A, middle, last),
-                   overlap)
 def alg3(A):#helper function
     result = divcon(A, 0, len(A))
     return(A[result[1]:result[2]], result[0])
 
 
-# myArray=[31, -41, 59, 26, -53, 58, 97, -93, -23, 84]
+#myArray=[31, -41, 59, 26, -53, 58, 97, -93, -23, 84]
 #
-# print alg3(myArray)
+#print alg3(myArray)
